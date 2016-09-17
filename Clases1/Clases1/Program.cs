47acd6a214b3e2149d3754e0ea3b0baf0d1f343e
Ramironacho <ramiro_nacho@hotmail.com>
2016-09-17 20:15:25 -0300

    Mensaje de commit

---
+++ Clases1/Clases1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Medicamento baya = new Medicamento();
+            Console.WriteLine("Ingrese Nombre del medicamento que desea");
+            String nombre = Console.ReadLine();
+            baya.MostrarMensaje(nombre);
+        }
+    }
+}
