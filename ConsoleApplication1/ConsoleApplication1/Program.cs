e008eb0c9b8006e340daff43e04119e7a9192f80
Ramironacho <ramiro_nacho@hotmail.com>
2016-09-17 21:14:10 -0300

    Subiendo lo que teniamos desde casa

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,24 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Probando GIT")
+            int numero1;
+            int numero2;
+            Console.WriteLine("Escriba el primer numero: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escriba el segundo numero: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            
+            if (numero1 == numero2)
+                Console.WriteLine("Los numeros son iguales");
+            if (numero1 != numero2)
+                Console.WriteLine("Los numeros son distinos");
+            if (numero1 > numero2)
+                Console.WriteLine("{0} es mayor que {1}", numero1,numero2);
+            if (numero1 < numero2)
+                Console.WriteLine("Los {0} es menor que {1}",numero1,numero2);
+            
+            Console.ReadKey();
         }
     }
 }
