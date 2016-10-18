fe594c001e59be85f68e00c2f1a26951fde79f8c
Ramironacho <ramiro_nacho@hotmail.com>
2016-10-17 22:23:38 -0300

    desde el dpto

--- Primeros Ejercicios/Primeros Ejercicios/Program.cs
+++ Primeros Ejercicios/Primeros Ejercicios/Program.cs
@@ -10,28 +10,45 @@ namespace Primeros_Ejercicios
     {
         static void Main(string[] args)
         {
-            int numeroEjercicio;
+            char salir ='s';
+            //int numeroEjercicio;
             EjerciciosSimples ej = new EjerciciosSimples();
             InterfaceG menu = new InterfaceG();
-            menu.Menu();
-            Console.WriteLine("Ingrese el numero de Ejercicio: ");
-            numeroEjercicio = Convert.ToInt32(Console.ReadLine());
-            if (numeroEjercicio == 1)
-                ej.Ejercicio1();
-            else if (numeroEjercicio == 2)
-                ej.Ejercicio2();
-            else if (numeroEjercicio == 3)
-                ej.Ejercicio3();
-            else if (numeroEjercicio == 4)
-                ej.Ejercicio4();
-            else if (numeroEjercicio == 5)
-                ej.Ejercicio5();
-            else if (numeroEjercicio == 6)
-                ej.Ejercicio6();
-            else
+            
+            //Console.WriteLine("Ingrese el numero de Ejercicio: ");
+
+            //numeroEjercicio = Convert.ToInt32(Console.ReadLine());
+            while (salir != 'n')
             {
-                Console.WriteLine("Numero Invalido");
-                Console.ReadKey();
+                menu.Menu();
+
+                if (menu.OpcionMenu == 1)
+                    ej.Ejercicio1();
+                else if (menu.OpcionMenu == 2)
+                    ej.Ejercicio2();
+                else if (menu.OpcionMenu == 3)
+                    ej.Ejercicio3();
+                else if (menu.OpcionMenu == 4)
+                    ej.Ejercicio4();
+                else if (menu.OpcionMenu == 5)
+                    ej.Ejercicio5();
+                else if (menu.OpcionMenu == 6)
+                    ej.Ejercicio6();
+                else if (menu.OpcionMenu == 7)
+                    salir = 'n';
+                else
+                {
+                    Console.WriteLine("Numero Invalido");
+                    Console.ReadKey();
+
+                }
+                if (salir == 's')
+                {
+                    Console.WriteLine("Desea continuar?: s/n");
+                    salir = Convert.ToChar(Console.ReadLine());
+                }
+
+                
             }
                 
 
