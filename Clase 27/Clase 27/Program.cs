76929e6f555ea58b748a1bf567b377f18637cadd
Ramiro Ignacio Fernandez <ramiro.fernandez@globant.com>
2017-05-08 17:34:02 -0300

    wweee

--- Clase 27/Clase 27/Program.cs
+++ Clase 27/Clase 27/Program.cs
@@ -10,6 +10,20 @@ namespace Clase_27
     {
         static void Main(string[] args)
         {
+            tiempo1 tiempo = new tiempo1();
+            Console.Write("La hora universal inicial es: ");
+            Console.WriteLine(tiempo.AstringUniversal());
+            Console.Write("La hora inicial estandar es:");
+            Console.WriteLine(tiempo.ToString());
+            Console.WriteLine();
+
+            tiempo.EstablecerTiempo(15, 27, 9);
+            Console.Write("La hora universal inicial es: ");
+            Console.WriteLine(tiempo.AstringUniversal());
+            Console.Write("La hora inicial estandar es:");
+            Console.WriteLine(tiempo.ToString());
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
